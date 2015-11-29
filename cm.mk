015a60ba5564a773657f7b16db7f640d03704874
vm03 <vm03@yandex.ru>
2015-11-29 20:21:46 +0300

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,14 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 320
+TARGET_SCREEN_HEIGHT := 480
+
+# Release name
+PRODUCT_RELEASE_NAME := LG L40
+PRODUCT_NAME := cm_w3ds
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := w3ds
+
+$(call inherit-product, device/lge/w3ds/full_w3ds.mk)
